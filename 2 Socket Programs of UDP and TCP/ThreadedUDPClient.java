c1f0e24dcb041f0f4a988724e5ab16b8491c4d8d
06_Kuldip Chauhan <kuldipchauhan000741@gmail.com>
2022-08-13 21:35:01 +0530

    Added Basic Programs and Socket Programs of TCP and UDP

---
+++ 2 Socket Programs of UDP and TCP/ThreadedUDPClient.java
@@ -0,0 +1,89 @@
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+
+import data.Connection;
+import data.Packet;
+import data.PacketHandler;
+
+public class ThreadedUDPClient implements Runnable {
+	
+	private Connection connect;
+	private boolean running;
+	
+	private DatagramSocket socket;
+	private Thread process, send, receive;
+	
+	public ThreadedUDPClient(String addr, int port) {
+		try {
+			socket = new DatagramSocket();
+			connect = new Connection(socket, InetAddress.getByName(addr), port, 0);
+			this.init();
+		} catch (SocketException | UnknownHostException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * Initialise the client
+	 */
+	private void init() {
+		process = new Thread(this, "server_process");
+		process.start();
+	}
+	
+	/**
+	 * Send some data
+	 * @param the data
+	 */
+	public void send(final byte[] data) {
+		send = new Thread("Sending Thread") {
+			public void run() {
+				connect.send(data);
+			}
+		};
+		
+		send.start();
+	}
+	
+	/**
+	 * Receive data on the given server connection
+	 */
+	public void receive(final PacketHandler handler) {
+		receive = new Thread("receive_thread") {
+			public void run() {
+				while(running) {
+					byte[] buffer = new byte[1024];
+					DatagramPacket dgpacket = new DatagramPacket(buffer, buffer.length);
+					
+					try {
+						socket.receive(dgpacket);
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+					
+					handler.process(new Packet(dgpacket.getData(), dgpacket.getAddress(), dgpacket.getPort()));
+				}
+			}
+		};
+		
+		receive.start();
+	}
+	
+	/**
+	 * Close the current connection for this client
+	 */
+	public void close() {
+		connect.close();
+		running = false;
+	}
+	
+	@Override
+	public void run() {
+		running = true;
+	}
+
+}
